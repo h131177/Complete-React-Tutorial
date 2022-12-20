587b72da3e45fc8fb2d0eb74e3ae92d60804595d
h131177 <54102543+h131177@users.noreply.github.com>
2022-12-21 00:05:32 +0100

    Using event parameter

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,18 @@
 const Home = () => {
 
-    const handleClick = () => {
-        console.log('hello, ninjas');
+    const handleClick = (e) => {
+        console.log('hello, ninjas', e);
     }
 
-    const handleClickAgain = (name) => {
-        console.log('hello ' + name);
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e);
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={() => handleClickAgain('mario')}>Click me again</button>
+            <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
         </div>
     );
 }
