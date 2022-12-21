3958f3784e02ebf03d3a64d6f19eff73a0038283
h131177 <54102543+h131177@users.noreply.github.com>
2022-12-21 12:32:12 +0100

    Variable not reactive

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,15 @@
 const Home = () => {
+    let name = 'mario';
 
     const handleClick = () => {
-        
+        name = 'luigi';
+        console.log(name)
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name }</p>
             <button onClick={handleClick}>Click me</button>
         </div>
     );
